d79732b9b52f0e6cd5817b6cfd0622d2558a6cda
geek-sajjad <sajjad.sohrabi2@gmail.com>
2022-09-29 01:47:07 +0330

    add vue-router

--- src/components/nav/TheNavigation.vue
+++ src/components/nav/TheNavigation.vue
@@ -2,27 +2,12 @@
   <header>
     <nav>
       <ul>
-        <li>
-          <button @click="setActivePage('teams-list')">Teams</button>
-        </li>
-        <li>
-          <button @click="setActivePage('users-list')">Users</button>
-        </li>
+        <li><router-link to="/teams">Teams</router-link></li>
+        <li><router-link to="users">Users</router-link></li>
       </ul>
     </nav>
   </header>
 </template>
-
-<script>
-export default {
-  emits: ['set-page'],
-  methods: {
-    setActivePage(page) {
-      this.$emit('set-page', page);
-    },
-  },
-};
-</script>
 
 <style scoped>
 header {
@@ -49,8 +34,8 @@ li {
   margin: 0 2rem;
 }
 
-button {
-  font: inherit;
+a {
+  text-decoration: none;
   background: transparent;
   border: 1px solid transparent;
   cursor: pointer;
@@ -59,8 +44,8 @@ button {
   display: inline-block;
 }
 
-button:hover,
-button:active {
+a:hover,
+a:active {
   color: #f1a80a;
   border-color: #f1a80a;
   background-color: #1a037e;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 
-const app = createApp(App)
+const app = createApp(App);
+const router = createRouter({
+  routes: [
+    {
+      path: '/teams',
+      component: TeamsList,
+    },
+    {
+      path: '/users',
+      component: UsersList,
+    },
+  ],
+  history: createWebHistory(),
+});
 
+app.use(router);
 app.mount('#app');
